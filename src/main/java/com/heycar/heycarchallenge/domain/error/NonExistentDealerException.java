b163a466a08f8136d17560f0a28e82d4df92b9f0
Bradley Morris <bradleymorrisza@gmail.com>
2019-03-10 12:37:48 +0200

    Fixed error message to actually display id

--- src/main/java/com/heycar/heycarchallenge/domain/error/NonExistentDealerException.java
+++ src/main/java/com/heycar/heycarchallenge/domain/error/NonExistentDealerException.java
@@ -2,6 +2,6 @@ package com.heycar.heycarchallenge.domain.error;
 
 public class NonExistentDealerException extends RuntimeException{
     public NonExistentDealerException(Long id){
-        super(String.format("No dealer with id: {} exists", id));
+        super(String.format("No dealer with id: %s exists", id));
     }
 }
